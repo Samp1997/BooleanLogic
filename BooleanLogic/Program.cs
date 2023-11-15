266e31bbe0a381346087929a7a8831c2cc44a6ae
Samp1997 <sampiazza2016@gmail.com>
2023-11-14 20:49:07 -0800

    Add project files.

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int person1Age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUIq1 = false;
+            
+            Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int person1St = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            Console.WriteLine("Qualified?");
+            
+
+            int age = 18;
+            int Q1pass = person1Age;
+
+            bool canDrive = (person1Age >= 16);
+            Console.WriteLine(canDrive);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
