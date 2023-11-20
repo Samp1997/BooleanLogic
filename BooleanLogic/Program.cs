b90f3a9ee18190884e4e11c6a5b9e3628a87d0f0
Samp1997 <sampiazza2016@gmail.com>
2023-11-20 14:31:14 -0800

    changes

--- BooleanLogic/Program.cs
+++ BooleanLogic/Program.cs
@@ -9,21 +9,29 @@ namespace BooleanLogic
             Console.WriteLine("What is your age?");
             int person1Age = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
-            Console.WriteLine("Have you ever had a DUI?");
-            bool DUIq1 = false;
-            
+            Console.WriteLine("Have you ever had a DUI? (True or False)");
+            bool answer = Convert.ToBoolean( Console.ReadLine().ToLower());
+
+
             Console.ReadLine();
             Console.WriteLine("How many speeding tickets do you have?");
             int person1St = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
             Console.WriteLine("Qualified?");
+
+            Console.WriteLine(person1Age > 15 && !answer && person1St < 4);
             
 
-            int age = 18;
-            int Q1pass = person1Age;
+            //int Q1pass = person1Age;
+            //int Q2pass = answer;
+            //int Q3pass = person1St;
 
-            bool canDrive = (person1Age >= 16);
-            Console.WriteLine(canDrive);
+            //bool speedingTic = (person1St < 3);
+            //bool ofAge = (person1Age >= 16);
+            //bool noDUI = (answer);
+
+            
+            Console.WriteLine();
 
 
 
